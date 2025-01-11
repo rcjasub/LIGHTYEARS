5cfe525063509325e366f23d7a8bef25493f3ba1
Jocsan Rodriguez <jasub270@gmail.com>
2025-01-11 09:25:01 -0500

    rename folder

--- LightYearsGame/include/config.h
+++ LightYearsGame/include/config.h
@@ -7,6 +7,6 @@ std::string GetResourceDir()
 #ifdef NDEBUG //release build
 	return "assets/";
 #else 
-	return "/tmp/jocsan_lightyears/LightYearsGame/assets/";
+	return "C:/lightyears_fix/LightYearsGame/assets/";
 #endif
 }
